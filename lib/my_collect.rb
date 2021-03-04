4f4de560bb27c95fd1f58c0d37336811a1223394
sumeikom <sumeikom@gmail.com>
2021-03-04 00:35:21 -0500

    commit tree 794d6ca594d41d483bcbd26784242c4f3317888c

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+        collection << yield(array[i])
+        i += 1
+    end
+
+    return collection
+
+    #array.collect do |language|
+    #    "#{language.upcase!}"
+    #end
+    #array.collect do |names|
+     #   "#{names.split{" ").first}"
+    #end
+end
+
 
